ee3f4de4cef1a8640fa9f4f34e1ff5e2e18c504f
aomzin <winittra_saengsroy@cmu.ac.th>
2021-12-21 16:52:44 +0700

    lab12finished

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,22 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+using namespace std ;
+
+
+int main ( ) {
+
+    srand(time(0));
+    string Q[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"} ;
+    string y = Q[rand()%9+1] ;
+
+    cout << "Press Enter 3 times to reveal your future.\n"; 
+    cin.get() ;
+    cin.get() ;
+    cin.get() ;
+    
+    cout << "You will get " << y << " in this 261102.";
+    
+    return 0 ;
+}
